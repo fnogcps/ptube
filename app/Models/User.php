8f2889606dbaf22abe1c25a4334b326cd647d1d6
Felipe Nogueira <contato.fnog@gmail.com>
2024-02-27 00:21:08 -0300

    feat: create user model

---
+++ app/Models/User.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace fnogcps\Ptube\Models;
+
+use fnogcps\Ptube\DBConnection;
+use PDO;
+use PDOException;
+
+class User
+{
+    private object $db;
+    private object $model;
+
+    public function __construct(DBConnection $db = new DBConnection())
+    {
+        $this->db = $db->getInstance();
+    }
+
+    public function create(string $user, string $name, string $pwd)
+    {
+        $q = $this->db->prepare('INSERT INTO users (user, name, password) VALUES (:user, :name, :pwd)');
+        $q->bindValue('user', $user, PDO::PARAM_STR);
+        $q->bindValue('name', $name, PDO::PARAM_STR);
+        $q->bindValue('pwd', $pwd, PDO::PARAM_STR);
+        $q->execute();
+    }
+
+    public function delete(string $user)
+    {
+        $q = $this->db->prepare('DELETE FROM users WHERE (user = :user)');
+        $q->bindValue('user', $user, PDO::PARAM_STR);
+        $q->execute();
+    }
+
+    public function get(string $user)
+    {
+        $q = $this->db->prepare('SELECT * FROM users WHERE (user = :user)');
+        $q->bindValue('user', $user, PDO::PARAM_STR);
+        $q->execute();
+        return $q->fetchAll();
+    }
+}
