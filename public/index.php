a83a44f9a87fcee1140c1987c11fd77d96f4afa8
Felipe Nogueira <contato.fnog@gmail.com>
2024-02-27 23:03:00 -0300

    feat/php: add Slim router

--- public/index.php
+++ public/index.php
@@ -2,18 +2,25 @@
 
 declare(strict_types=1);
 
-namespace fnogcps\Ptube;
-
 require __DIR__ . '/../vendor/autoload.php';
 
-use fnogcps\Ptube\UserController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteContext;
 
-$router = new \Bramus\Router\Router();
+use fnogcps\Ptube\Controllers\UserController;
 
-$router->setNamespace('fnogcps\Ptube\Controllers');
+$app = AppFactory::create();
 
-// $router->get('/c/([a-z0-9-]+)', 'UserController@createUser');
-// $router->get('/d/([a-z0-9-]+)', 'UserController@deleteUser');
-// $router->get('/g/([a-z0-9-]+)', 'UserController@getUser');
+$app->post('/create', static function (Request $req, Response $res) {
+    (new UserController())->createUser($req->getParsedBody());
+    return $res->withHeader('Content-Type', 'application/json');
+});
 
-$router->run();
+$app->post('/login', static function (Request $req, Response $res) {
+    (new UserController())->getUser($req->getParsedBody());
+    return $res->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
