438d164275195ca1e82ec68f4480e95e2945497b
odmrs <omarcosviniciusdev@gmail.com>
2024-03-03 22:36:08 -0300

    feat(vue): add proxy to routes

--- public/src/view/Dashboard.vue
+++ public/src/view/Dashboard.vue
@@ -24,7 +24,7 @@
                 stroke-width="2"
                 d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
             </svg>
-            <span>Sign in</span>
+            <span>Sign up</span>
           </router-link>
           <router-link
             to="/login"
--- public/vite.config.js
+++ public/vite.config.js
@@ -3,5 +3,15 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 4000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        ws: true,
+      }
+    }
+  }
 });
